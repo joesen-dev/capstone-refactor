c1aa7b80540efb7b0b3aeadaf853844a6bb06785
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-10-25 14:22:16 -0400

    [ISSUE #1] - refactored Nav to add mockup content

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,7 +8,13 @@ function Nav() {
 			return (
 				<ul className="flex-row">
 					<li className="mx-1">
-						<Link to="/orderHistory">Order History</Link>
+						<Link to="/orderHistory">Dashboard</Link>
+					</li>
+					<li className="mx-1">
+						<Link to="/orderHistory">Workouts</Link>
+					</li>
+					<li className="mx-1">
+						<Link to="/orderHistory">Profile</Link>
 					</li>
 					<li className="mx-1">
 						{/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -39,7 +45,7 @@ function Nav() {
 					<span role="img" aria-label="shopping bag">
 						🛍️
 					</span>
-					-Shop-Shop
+					Capstone
 				</Link>
 			</h1>
 
